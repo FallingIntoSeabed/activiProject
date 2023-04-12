764be4eb93075e967382ee439bd126f35cc017b8
jiangfusheng <1913008715@qq.com>
2023-04-12 19:41:57 +0800

    fist commit

---
+++ src/main/java/com/example/activiproject/ActiviProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.activiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiviProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiviProjectApplication.class, args);
+    }
+
+}
